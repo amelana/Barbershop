a428522cdfd75788c13dd7fd16bdcad914bb0648
Svet Lana <svetylya6m@gmail.com>
2020-06-06 19:04:12 +0300

    remove file

--- js/lib.js
+++ js/lib.js
@@ -7,14 +7,6 @@
 });
 
 
- $('.header').on('click','.login-button',function(){
- 	$('#login-modal').fadeIn();
- });
-
- $('.modal__window').on('click','.modal__close',function(){
- 	$('#login-modal').fadeOut();
- });
-
    var mySwiper = new Swiper ('.swiper-container', {
   loop: true,
 
